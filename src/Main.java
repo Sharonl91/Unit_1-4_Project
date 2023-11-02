1979dd189a27933d6a8686f0e76ee94a8d56c8d3
BT_4N2_11 <mikm@>
2023-11-02 14:03:07 -0400

    modified a bunch of codes to ensure that my code would actually compile instead of ending after the first choice was selected, created the monster object and player object in the Player class, and created a method that checks the player's input is valid instead of repeating the same code everytime a choice has to be inputted.

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,37 @@
 import java.util.Scanner;
 public class Main {
+    public static final Scanner s = new Scanner(System.in);
+    public static String c;
+
     public static void main(String[] args) {
-        final Scanner s = new Scanner(System.in);
-        System.out.println("You are a slave living in the middle East. You have been tortured for as long as you can remember. Your master had just left for the hospital with his son and told you to watch the house. \nHe forgot to lock the windows, you decide to: \n(run away or stay put).");
-        String c = s.nextLine();
-        while (!(c.equals("run away") || c.equals("stay put"))){
-            System.out.println("Please enter a valid option:");
+        Player p = new Player();
+        System.out.println("\nYou are a slave living in the middle East. You have been tortured for as long as you can remember. Your master had just \nleft for the hospital with his son and told you to watch the house. He forgot to lock the windows, you decide to: \n(choose run away or stay put).");
+        c = s.nextLine();
+        p.checkChoice("run away","stay put");
+        if (c.equals("stay put")){
+            System.out.println("You waited until late evening, when your master finally came home. In his arms is the corpse of his son. As soon as he saw you, he \nlaid the corpse on the sofa and walked toward you. He started kicking and stomping on you while shouting \"Why are you still alive when my son is gone? You piece of filth\".\nBecause you ate nothing the entire day and could not endure your master's abuse, you died.\nTHE END");
+        }
+        if (c.equals("run away")){
+            System.out.println("You escaped from the place you had lived in for over a decade. The future looks both bright and bleak.\nYou see a forest to the east and the town you frequented to the west. Where will you go now: \n(choose east or west)");
             c = s.nextLine();
-        }
-        if (c == "stay put"){
-            System.out.println("You waited until late evening, when your master finally came home. In his arms is the corpse of his son. As soon as he saw you, he laid the corpse on the sofa and walked toward you. He started kicking and stomping on you while shouting \"Why are you still alive when my son is gone? You piece of filth\".\n Because you ate nothing the entire day and could not endure your master's abuse, you died.\n THE END");
-        }
-        if (c == "run away"){
-            System.out.println("You escaped from the place you had lived in for over a decade. The future looks both bright and bleak.\n You see a forest to the east and the town you frequented to the west. Where will you go now: \n (east or west)");
-            while (!(c.equals("east") || c.equals("west"))){
-                System.out.println("Please enter a valid option:");
+            p.checkChoice("east","west");
+            if (c.equals("east")){
+                System.out.println("You set foot into an unknown terrain. While stumbling around, you accidentally fell into a pit. \nAs you ponder what to do, you saw a shadow loom overhead. You see a: \n(choose living thing or nonliving thing)");
+                c = s.nextLine();
+                p.checkChoice("living thing","nonliving thing");
+                if (c.equals("living thing")){
+                    System.out.println("You see a bear above you and before you can react, it had already jumped down and ate you.\nTHE END");
+                }
+                if (c.equals("nonliving thing")){
+                    System.out.println("You see some ropes dangling from a tree above you and wonder if someone left it there to help humans that accidentally got trapped. \nEither way, you're grateful for its existence and climbed out of the wh");
+                    c = s.nextLine();
+                }
+            }
+            if (c.equals("west")){
+                System.out.println("It has been one week. Without your master, you could only eat some nuts on the way to the city. You ended up on the living on the streets and an old man offers to take you home. You:\n(refuse or accept)");
                 c = s.nextLine();
             }
-            if (c == "east"){
-                System.out.println("You set fot into an unknown terrain.");
-
-            }
-            else if (c == "west"){
-                System.out.println("It has been one week. Without your master, you could only eat some nuts on the way to the city. You ended up on the living on the streets and an old man offers to take you home. You:\n(refuse or accept)");
-            }
+            p.checkChoice("refuse","accept");
         }
 
     }
--- src/Player.java
+++ src/Player.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
 
 public class Player {
-    private String name;
-    private int l = 1;
-    private int h = 100;
+    public static final Scanner s = new Scanner(System.in);
+    int l;
+    int h;
+    int patk = 0;
+    int matk = 0;
 
-    public String toString() {
-        return name + " is currently level " + l + " and has " + h + " hp left.";
+    public Player(int level, int hp){
+        l = level;
+        h = hp;
+        patk = (int)(Math.random()*10)+level;
     }
+
+    public Monster(int level){
+        level = (int)(Math.random()*l);
+        matk = (int)(Math.random()*11)+level;
+    }
+
+    public String checkChoice(String choice1, String choice2) {
+        while (!(Main.c.equals(choice1) || Main.c.equals(choice2))) {
+            System.out.println("Please enter a valid option:");
+            Main.c = s.nextLine();
+        }
+        return Main.c;
+    }
+
 }
+
