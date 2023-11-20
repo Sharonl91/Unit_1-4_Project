f7af98b3515aadc9961130beb895c69870f7c700
BT_LIBRARY_08 <@vjh>
2023-11-20 14:51:11 -0500

    I changed and modified basically everything in the player class because some of the methods had print statements in it. I changed them into return and had to fix the checkChoice method to ensure that even after the user entered an invalid choice, the program will prompt for a valid option and continue  accepting the next line using the scanner utility. For some reason the checkChoice method worked on my personal computer but it doesn't commit so I have to do that on the school pc, where the code doesn't work for some reason.

--- src/Main.java
+++ src/Main.java
@@ -1,69 +1,77 @@
-import java.util.Scanner;
 public class Main {
-    public static final Scanner s = new Scanner(System.in);
     public static String c;
-
     public static void main(String[] args) {
-        Player p = new Player(2,300);
-        System.out.println("\nYou are a slave living in city of Atlantis. You have been tortured for as long as you can remember. Your master had just " +
-                "\nleft for the hospital with his son and told you to watch the house. He forgot to lock the windows, you decide to: " +
+        //Starts off the game by creating an Player object
+        Player p = new Player(6, 100);
+        System.out.println("\nYou are a slave living in the city of Atlantis. You have been tortured for as long as you can remember. Your master had " +
+                "\njust left for the hospital with his son and told you to watch the house. He forgot to lock the windows, you decide to: " +
                 "\n(choose run away or stay put).");
-        c = s.nextLine();
-        p.checkChoice("run away","stay put");
-        if (c.equals("stay put")){
-            System.out.println("You waited until late evening, when your master finally came home. In his arms is the corpse of his son. As soon as he saw you, he " +
-                    "\nlaid the corpse on the sofa and walked toward you. He started kicking and stomping on you while shouting \"Why are you still alive when my son is gone? You piece of filth\"." +
-                    "\nBecause you ate nothing the entire day and could not endure your master's abuse, you died.\nTHE END");
+        p.checkChoice("run away", "stay put");
+        if (c.equalsIgnoreCase("stay put")) {
+            System.out.println("\nYou waited until late evening, when your master finally came home. In his arms is the corpse of his son. As soon " +
+                    "\nas he saw you, he laid the corpse on the sofa and walked toward you. He started kicking and stomping on you while " +
+                    "\nshouting. Why are you still alive when my son is gone? You piece of filth. Because you ate nothing the entire day " +
+                    "\nand could not endure your master's abuse, you died.\nTHE END...");
         }
-        if (c.equals("run away")){
-            System.out.println("You escaped from the place you had lived in for over a decade. The future looks both bright and bleak." +
-                    "\nYou see a forest to the east and the town you frequented to the west. Where will you go now: \n(choose east or west)");
-            c = s.nextLine();
-            p.checkChoice("east","west");
-            if (c.equals("east")){
-                System.out.println("You set foot into an unknown terrain. While stumbling around, you accidentally fell into a pit. " +
-                        "\nAs you ponder what to do, you saw a shadow loom overhead. You see a: \n(choose living thing or nonliving thing)");
-                c = s.nextLine();
-                p.checkChoice("living thing","nonliving thing");
-                if (c.equals("living thing")){
-                    System.out.println("You see a bear above you and before you can react, it had already jumped down and ate you.\nTHE END");
+        if (c.equalsIgnoreCase("run away")) {
+            System.out.println("\nYou escaped from the place you had lived in for over a decade. The future looks both bright and bleak." +
+                    "\nYou see a forest to the east and the town you frequented to the west. Where will you go now: " +
+                    "\n(choose east or west)");
+            p.checkChoice("east", "west");
+            if (c.equalsIgnoreCase("east")) {
+                System.out.println("\nYou set foot into an unknown terrain. While stumbling around, you accidentally fell " +
+                        "\ninto a pit. As you ponder what to do, you saw a shadow loom overhead. You see a: " +
+                        "\n(choose living thing or nonliving thing)");
+                p.checkChoice("living thing", "nonliving thing");
+                if (c.equalsIgnoreCase("living thing")) {
+                    System.out.println("\nYou see a bear above you and before you can react, it had already jumped down and ate you.\nTHE END");
+                    System.exit(15);
+                } else if (c.equalsIgnoreCase("nonliving thing")) {
+                    System.out.println("\nYou see some ropes dangling from a tree above you and wonder if someone left it there to help " +
+                            "\nhumans that accidentally got trapped. Either way, you're grateful for its existence and climbed out of " +
+                            "\nthe pit. As you explore deeper, you encountered something.\n");
+                    System.out.println(p.monsterAppears());
+                    System.out.println("\nWhat will you do now: \n(choose attack or run)");
+                    p.checkChoice("attack", "run");
+                    if (c.equalsIgnoreCase("attack")) {
+                        while (true) {
+                            String attackResult = p.attack();
+                            System.out.println(attackResult);
+                            if (attackResult.contains("!")) {
+                                break;  // Exit the loop if the battle is over
+                            }
+                        }
+                        System.out.println("\nYou realized that the monster you just defeated was the king of this forest. " +
+                                "\nNow that you've defeated it, you became the new ruler of this forest.\nTHE END!");
+                        System.exit(15);
+                    } else if (c.equalsIgnoreCase("run")) {
+                        System.out.println("\nYou quickly turned tail and ran from the monster, little did you know that it's " +
+                                "\nspeed is way faster than you. It caught up to you in seconds and slaughtered you.\nTHE END!");
+                        System.exit(15);
+                    }
+                }
+            } else if (c.equalsIgnoreCase("west")) {
+                System.out.println("\nIt has been one week. Without your master, you could only eat some nuts on the way to the " +
+                        "\ncity. You ended up on the living on the streets and an old man offers to take you home. You:" +
+                        "\n(choose refuse or accept)");
+                p.checkChoice("refuse", "accept");
+                if (c.equalsIgnoreCase("refuse")) {
+                    System.out.println("\nThe old man decides to leave you alone and threw a loaf of bread down. You were grateful for the help " +
+                            "\nand swore to repay it somehow in the future. You decided to work for any store that will accept you. " +
+                            "\nAfter you earned enough to make a living, you decided to invest your savings and bought a mansion. " +
+                            "\nYou encountered the old man that helped you, and hired him to be your butler. You couldn't find it in " +
+                            "\nyourself to marry and start a proper family so you adopted a child. All in all, you lived a wonderful life " +
+                            "\nat your last moment! You opened your eyes all of a sudden and it turns out you were dreaming this entire time. " +
+                            "\nYou never escaped and you're trapped forever! \nTHE END!");
+                    System.exit(15);
+                } else if (c.equalsIgnoreCase("accept")) {
+                    System.out.println("\nYou decided to follow the old man home and became his child. You studied in school and became " +
+                            "\na brilliant person. In the next decade, you became a young and rich merchant who had everything " +
+                            "\na person could want. You bought a house and is living a peaceful life with your spouse. " +
+                            "\nYou even have a son who's both humble and wise. You were able to live a life filled with " +
+                            "\nparental love and successes. \nTHE END!");
                     System.exit(15);
                 }
-                else if (c.equals("nonliving thing")){
-                    System.out.println("You see some ropes dangling from a tree above you and wonder if someone left it there to help humans that accidentally got trapped. " +
-                            "\nEither way, you're grateful for its existence and climbed out of the pit. As you explore deeper, you encountered a monster.");
-                    p.monsterAppears();
-                    System.out.println("What will you do now: (choose attack or run)");
-                    c = s.nextLine();
-                    p.checkChoice("attack","run");
-                    if (c.equals("attack")){
-                        p.attack();
-                        System.out.println("You realized that the monster you just defeated was the king of this forest. Now that you've defeated it, you became the new ruler of this forest.\nTHE END!");
-                        System.exit(15);
-                    }
-                    else if (c.equals("run")){
-                        System.out.println("You quickly turned tail and ran from the monster, little did you know that it's speed is way faster than you." +
-                                "\nIt caught up to you in seconds and slaughtered you.\nTHE END!");
-                        System.exit(15);
-                    }
-                }
-            }
-            else if (c.equals("west")){
-                System.out.println("It has been one week. Without your master, you could only eat some nuts on the way to the city. You ended up on the living on the streets and an old man offers to take you home. You:\n(refuse or accept)");
-                c = s.nextLine();
-            }
-            p.checkChoice("refuse","accept");
-            if (c.equals("refuse")){
-                System.out.println("The old man decides to leave you alone and threw a loaf of bread down. You were grateful for the help and swore to repay it somehow in the future." +
-                        "\nYou decided to work for any store that will accept you. In the next decade, you became a young and rich merchant who had everything a person could want." +
-                        "\nYou bought a house and is living a peaceful life with your spouse. You encountered the old man that helped you, and hired him to be your butler." +
-                        "\nYou even have a son who's both humble and wise. All in all, you lived a happy life at the end and died with your family! " +
-                        "\nSadly it turns out you were dreaming this entire time, you never escaped and you are trapped forever! THE END!");
-                System.exit(15);
-            }
-            else if (c.equals("accept")){
-                System.out.println("You decided to follow the old man home and became his child. You studied in school and became a brilliant person. You were able to live a life filled with parental love and successes. \nTHE END!");
-                System.exit(15);
             }
         }
     }
--- src/Player.java
+++ src/Player.java
@@ -1,11 +1,10 @@
 import java.util.Scanner;
-
 public class Player {
     public static final Scanner s = new Scanner(System.in);
     //level
     private int l;
     //health points
-    private double h;
+    public double h;
     //player attack damage
     private int patk = 0;
     //monster level
@@ -20,48 +19,47 @@ public class Player {
         h = hp;
     }
     //the following method allows me to check if the player entered a valid choice
-    //without repeating the code multiple times throughout the Main class
+    //without the need to type all these code multiple times throughout the Main class
     public String checkChoice(String c1, String c2) {
-        while (!(Main.c.equals(c1) || Main.c.equals(c2))) {
-            System.out.println("Please enter a valid option:");
-            Main.c = s.nextLine();
+        while (true) {
+            Main.c = s.nextLine().trim();
+            if (Main.c.equalsIgnoreCase(c1) || Main.c.equalsIgnoreCase(c2)) {
+                break;  // Exit the loop if the choice is valid
+            } else {
+                return "Please enter a valid option: (" + c1 + "/" + c2 + ")";
+            }
         }
-        return Main.c;
-    }
-    //the following method returns the health point of the monster
-    public double getMhp() {
-        return mhp;
+        return ""; //this code won't run because either the if or the else statement will compile first
     }
     //the following method creates a chance encounter with a monster whose stats are randomized and based on the player level
     //the monster's stats corresponds with the player's level
-    public void monsterAppears() {
+    public String monsterAppears() {
         ml = (int) (Math.random() * l);
-        mhp = (0.95 * h);
-        System.out.println("A monster has appeared with " + mhp + " health.");
+        mhp = (int) (Math.random() * 0.95) + h;
+        return "A monster has appeared with " + mhp + " health.";
     }
-    //the following
-    public void attack() {
-        while (mhp > 0) {
-            patk = (int)(Math.random() * 20) + l;
-            matk = (int) (Math.random() * 15) + ml;
-            mhp = getMhp() - patk;
-            h -= matk;
-            System.out.println("You strike for " + patk);
-            System.out.println("You have been hit for " + matk);
-            if (mhp < 0){
-                mhp = 0;
-            }
-            if (h < 0){
-                h = 0;
-            }
-            System.out.println("You have " + h + " hp left.\nThe enemy has " + mhp + " hp left.");
+    //the following method copies the real-life situation of fighting against a monster and returns the amount being dealt to the monster,
+    //the amount received, and the remaining hp left for both the user and the monster
+    //when the hp drops to 0 or below for either side, a corresponding message will be returned and the game will continue on
+    public String attack() {
+        patk = (int) (Math.random() * 15) + l;
+        matk = (int) (Math.random() * 15) + ml;
+        mhp -= patk;
+        h -= matk;
+        String result = "You strike for " + patk + ".\nYou have been hit for " + matk +
+                ".\nYou have " + h + " hp left.\nThe enemy has " + mhp + " hp left.\n";
+        if (mhp <= 0) {
+            result+= "You survived the battle!";
+        } else if (h <= 0) {
+            result+= "You died. THE END!";
         }
-        if (h <= 0) {
-            System.out.println("You died. THE END!");
-            System.exit(15);
-        } else {
-            System.out.println("You survived the battle!");
+        try {
+            // Introduce a 2-second delay (2000 milliseconds) before returning the result
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            // Restore interrupted status
+            Thread.currentThread().interrupt();
         }
+        return result;  // Return the result of the attack
     }
 }
-
